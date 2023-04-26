da053e7a9e3dfd832d02889412d062f11fc34c8c
Faithful <faithfulolaleru09@gmail.com>
2023-04-26 18:32:30 +0100

    Cleanup 1.0

--- src/utils/makechain.ts
+++ src/utils/makechain.ts
@@ -1,11 +1,11 @@
-import { OpenAIChat } from 'langchain/llms';
-import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
-import { PineconeStore } from 'langchain/vectorstores';
-import { PromptTemplate } from 'langchain/prompts';
-import { CallbackManager } from 'langchain/callbacks';
+import { OpenAIChat } from "langchain/llms";
+import { LLMChain, ChatVectorDBQAChain, loadQAChain, ConversationalRetrievalQAChain } from "langchain/chains";
+import { PineconeStore } from "langchain/vectorstores";
+import { PromptTemplate } from "langchain/prompts";
+import { CallbackManager } from "langchain/callbacks";
 
 const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+    PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
@@ -25,36 +25,49 @@ Question: {question}
 Answer in Markdown:`,
 );
 
-export const makeChain = (
-  vectorstore: PineconeStore,
-  onTokenStream?: (token: string) => void,
-) => {
-  const questionGenerator = new LLMChain({
-    llm: new OpenAIChat({ temperature: 0 }),
-    prompt: CONDENSE_PROMPT,
-  });
-  const docChain = loadQAChain(
-    new OpenAIChat({
-      temperature: 0,
-      modelName: 'gpt-3.5-turbo', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
-      streaming: Boolean(onTokenStream),
-      callbackManager: onTokenStream
-        ? CallbackManager.fromHandlers({
-            async handleLLMNewToken(token) {
-              onTokenStream(token);
-              console.log(token);
-            },
-          })
-        : undefined,
-    }),
-    { prompt: QA_PROMPT },
-  );
+// const QA_PROMPT = PromptTemplate.fromTemplate(
+//     `You are to analyze bank statements for transactions frequency. Provide a conversational answer based on the context provided.
+// You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
+// If you can't find the answer in the context below, just say "Hmm, I'm not sure." Don't try to make up an answer.
+// If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
-  return new ChatVectorDBQAChain({
-    vectorstore,
-    combineDocumentsChain: docChain,
-    questionGeneratorChain: questionGenerator,
-    returnSourceDocuments: true,
-    k: 2, //number of source documents to return
-  });
+// Question: {question}
+// =========
+// {context}
+// =========
+// Answer in Markdown:`
+// );
+
+export const makeChain = (
+    vectorstore: PineconeStore,
+    onTokenStream?: (token: string) => void
+) => {
+    const questionGenerator = new LLMChain({
+        llm: new OpenAIChat({ temperature: 0 }),
+        prompt: CONDENSE_PROMPT,
+    });
+    const docChain = loadQAChain(
+        new OpenAIChat({
+            temperature: 0,
+            modelName: "gpt-3.5-turbo", //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+            streaming: Boolean(onTokenStream),
+            callbackManager: onTokenStream
+                ? CallbackManager.fromHandlers({
+                      async handleLLMNewToken(token) {
+                          onTokenStream(token);
+                          console.log(token);
+                      },
+                  })
+                : undefined,
+        }),
+        { prompt: QA_PROMPT }
+    );
+
+    return new ChatVectorDBQAChain({
+        vectorstore,
+        combineDocumentsChain: docChain,
+        questionGeneratorChain: questionGenerator,
+        returnSourceDocuments: true,
+        k: 2, //number of source documents to return
+    });
 };
